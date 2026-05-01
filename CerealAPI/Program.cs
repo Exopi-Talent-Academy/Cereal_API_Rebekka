b9955315e971c6c61442ea8790702a30868b7e02
RebekkaHeart <rebekkapahuspedersen@gmail.com>
2026-05-01 10:08:46 +0200

    Made and connected empty database

--- CerealAPI/Program.cs
+++ CerealAPI/Program.cs
@@ -1,4 +1,5 @@
 using Cereal_API.Repositories;
+using Microsoft.Data.Sqlite;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,6 +8,16 @@ builder.Services.AddRazorPages();
 
 // Register services:
 var services = builder.Services;
+
+try
+{
+    using var connection = new SqliteConnection(@"Data Source=C:\Dev\ExOpi\Cereal_API_Rebekka\Cereals.db");
+    connection.Open();
+} 
+catch (SqliteException ex)
+{
+    Console.WriteLine(ex.Message);
+}
 
 services.AddTransient<ICerealRepository, CerealRepository>();
 
