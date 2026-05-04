f3dff362fecb335341abaefdba9c2e9375a98233
RebekkaHeart <rebekkapahuspedersen@gmail.com>
2026-05-04 11:38:15 +0200

    Parser for CSV to my Cereal data type works

--- CerealAPI/Extensions/CsvToDataParser.cs
+++ CerealAPI/Extensions/CsvToDataParser.cs
@@ -1,5 +1,6 @@
 ﻿using Cereal_API.Models;
 using Cereal_API.Models.Types;
+using CsvHelper.Configuration;
 using System.Globalization;
 using System.Text.RegularExpressions;
 
@@ -9,43 +10,53 @@ public class CsvToDataParser
 {
     public static List<Cereal> CsvDataToCereals(string csvPath)
     {
-        try
+        if (!Regex.IsMatch(csvPath, @"^.+\.csv$"))
         {
-            Regex.Match(csvPath, @"^.+\.csv$");
-        }
-        catch (ArgumentException) 
-        { 
             throw new ArgumentException("The file given is not a csv-file.");
         }
 
         using var reader = new StreamReader(csvPath);
-        using var csv = new CsvHelper.CsvReader(reader, CultureInfo.InvariantCulture);
-        var records = csv.GetRecords<dynamic>();
+        var configuration = new CsvConfiguration(CultureInfo.InvariantCulture)
+        {
+            Delimiter = ";"
+        };
+        using var csv = new CsvHelper.CsvReader(reader, configuration);
+        var records = csv.GetRecords<dynamic>().ToList();
 
         List<Cereal> cereals = new List<Cereal>();
 
-        foreach (var record in records)
+        for (int i = 1; i < records.Count(); i++) // skip index 0 since it contains the column types
         {
-            Cereal cereal = new Cereal
-            (
-                Guid.NewGuid(),
-                record.name,
-                (Manufacturers)Enum.Parse(typeof(Manufacturers), record.mfr),
-                (HotOrColdType)Enum.Parse(typeof(HotOrColdType), record.type),
-                record.calories,
-                record.protein,
-                record.fat,
-                record.sodium,
-                float.Parse(record.fiber),
-                float.Parse(record.carbo),
-                record.sugars,
-                record.potass,
-                (VitaminAndMineralsType)Enum.Parse(typeof(VitaminAndMineralsType), record.vitamins),
-                (DisplayShelfType)Enum.Parse(typeof(DisplayShelfType), record.shelf),
-                float.Parse(record.weight),
-                float.Parse(record.cups),
-                float.Parse(record.rating)
-            );
+            Cereal cereal;
+
+            try
+            {
+                cereal = new Cereal
+                (
+                    Guid.NewGuid(),
+                    records[i].name,
+                    (Manufacturers)Enum.Parse(typeof(Manufacturers), records[i].mfr),
+                    (HotOrColdType)Enum.Parse(typeof(HotOrColdType), records[i].type),
+                    int.Parse(records[i].calories),
+                    int.Parse(records[i].protein),
+                    int.Parse(records[i].fat),
+                    int.Parse(records[i].sodium),
+                    float.Parse(records[i].fiber, CultureInfo.InvariantCulture.NumberFormat), // need to specify the culture so it parses the float with a dot instead of a comma
+                    float.Parse(records[i].carbo, CultureInfo.InvariantCulture.NumberFormat),
+                    int.Parse(records[i].sugars),
+                    int.Parse(records[i].potass),
+                    (VitaminAndMineralsType)Enum.Parse(typeof(VitaminAndMineralsType), records[i].vitamins),
+                    (DisplayShelfType)Enum.Parse(typeof(DisplayShelfType), records[i].shelf),
+                    float.Parse(records[i].weight, CultureInfo.InvariantCulture.NumberFormat),
+                    float.Parse(records[i].cups, CultureInfo.InvariantCulture.NumberFormat),
+                    float.Parse(records[i].rating, CultureInfo.InvariantCulture.NumberFormat)
+                );
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error parsing record at index {i}: {ex.Message}");
+                continue; // skip this record and move to the next one
+            }
 
             cereals.Add(cereal);
         }
--- CerealAPITests/Extensions/CsvToDataParserTests.cs
+++ CerealAPITests/Extensions/CsvToDataParserTests.cs
@@ -12,16 +12,25 @@ public sealed class CsvToDataParserTests
     {
         // Arrange
         string csvPath = "../../../TestData/valid_cereal_data.csv";
+
         string cereal1Name = "100% Bran";
         Manufacturers cereal1Mfr = Manufacturers.N;
+        int cereal1Calories = 70;
+        VitaminAndMineralsType cereal1Vitamins = VitaminAndMineralsType.TwentyFive;
         DisplayShelfType cereal1Shelf = DisplayShelfType.Three;
         float cereal1Rating = 68.402973f;
+
         string cereal2Name = "Apple Jacks";
         Manufacturers cereal2Mfr = Manufacturers.K;
+        int cereal2Calories = 110;
+        VitaminAndMineralsType cereal2Vitamins = VitaminAndMineralsType.Zero;
         DisplayShelfType cereal2Shelf = DisplayShelfType.Two;
         float cereal2Rating = 33.174094f;
+
         string cereal3Name = "Nut&Honey Crunch";
         Manufacturers cereal3Mfr = Manufacturers.K;
+        int cereal3Calories = 120;
+        VitaminAndMineralsType cereal3Vitamins = VitaminAndMineralsType.OneHundred;
         DisplayShelfType cereal3Shelf = DisplayShelfType.Two;
         float cereal3Rating = 29.924285f;
 
@@ -31,17 +40,51 @@ public sealed class CsvToDataParserTests
         // Assert
         Assert.IsNotNull(cereals);
         Assert.IsNotEmpty(cereals);
+
         Assert.AreEqual(cereal1Name, cereals[0].Name);
         Assert.AreEqual(cereal1Mfr, cereals[0].Mfr);
+        Assert.AreEqual(cereal1Calories, cereals[0].Calories);
+        Assert.AreEqual(cereal1Vitamins, cereals[0].Vitamins);
         Assert.AreEqual(cereal1Shelf, cereals[0].Shelf);
         Assert.AreEqual(cereal1Rating, cereals[0].Rating);
+
         Assert.AreEqual(cereal2Name, cereals[1].Name);
         Assert.AreEqual(cereal2Mfr, cereals[1].Mfr);
+        Assert.AreEqual(cereal2Calories, cereals[1].Calories);
+        Assert.AreEqual(cereal2Vitamins, cereals[1].Vitamins);
         Assert.AreEqual(cereal2Shelf, cereals[1].Shelf);
         Assert.AreEqual(cereal2Rating, cereals[1].Rating);
+
         Assert.AreEqual(cereal3Name, cereals[2].Name);
         Assert.AreEqual(cereal3Mfr, cereals[2].Mfr);
+        Assert.AreEqual(cereal3Calories, cereals[2].Calories);
+        Assert.AreEqual(cereal3Vitamins, cereals[2].Vitamins);
         Assert.AreEqual(cereal3Shelf, cereals[2].Shelf);
         Assert.AreEqual(cereal3Rating, cereals[2].Rating);
+    }
+
+    [TestMethod]
+    public void CsvDataToCereals_ShouldThrowArgumentException_WhenFileIsNotCsv()
+    {
+        // Arrange
+        string invalidFilePath = "../../../MSTestSettings.cs";
+
+        // Act & Assert
+        Assert.Throws<ArgumentException>(() => CsvToDataParser.CsvDataToCereals(invalidFilePath));
+    }
+
+    [TestMethod]
+    public void CsvDataToCereals_ShouldNotAddDataRow_WhenItHasInvalidData()
+    {
+        // Arrange
+        string csvPath = "../../../TestData/has_one_invalid_cereal_data.csv";
+
+        // Act
+        List<Cereal> cereals = CsvToDataParser.CsvDataToCereals(csvPath);
+
+        // Assert
+        Assert.IsNotNull(cereals);
+        Assert.IsNotEmpty(cereals);
+        Assert.HasCount(2, cereals);
     }
 }
