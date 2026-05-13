31f9203a47f6dfbf0a8a2c4c356c169a6c734319
RebekkaHeart <rebekkapahuspedersen@gmail.com>
2026-05-13 11:48:30 +0200

    Made frontend methods for setting the filter

--- CerealAPI/Pages/Cereal/ListCereals.cshtml.cs
+++ CerealAPI/Pages/Cereal/ListCereals.cshtml.cs
@@ -1,7 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Cereal_API.Controllers;
+using Cereal_API.Models.Types;
 using Cereal_API.Repositories;
-using Cereal_API.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Cereal_API.Pages.Cereal;
 
@@ -15,12 +16,46 @@ public class ListCerealsModel : PageModel
     }
 
     public IList<Models.Cereal> Cereal { get;set; } = default!;
+    private string FilterCategory { get; set; } = string.Empty;
+    private OperatorType FilterOperation { get; set; }
+    private string FilterValue { get; set; } = string.Empty;
+    // should have something that determines how it gets sorted
 
     public async Task OnGetAsync()
     {
-        //should take some inputs that determines how the cereals are sorted, filtered, etc. but for now just return all cereals ordered by name
-        var ok = await _controller.GetCereals();
-        var cereals = (ok.Result as ViewResult)?.Model as IEnumerable<Models.Cereal>;
-        Cereal = cereals!.OrderBy(c => c.Name).ToList();
+        if (FilterCategory == string.Empty || FilterValue == string.Empty)
+        {
+            var ok = await _controller.GetCereals();
+            var cereals = (ok.Result as ViewResult)?.Model as IEnumerable<Models.Cereal>;
+            Cereal = cereals!.OrderBy(c => c.Name).ToList();
+        } 
+        else
+        {
+            var ok = await _controller.GetCereals(FilterCategory, FilterOperation, FilterValue);
+            var cereals = (ok.Result as ViewResult)?.Model as IEnumerable<Models.Cereal>;
+            Cereal = cereals!.OrderBy(c => c.Name).ToList();
+        }
+    }
+
+    public async Task FilterCereals(string category, OperatorType operation, string value)
+    {
+        if (typeof(Models.Cereal).GetProperty(category) == null) { return; }
+
+        if (value != string.Empty)
+        {
+            FilterCategory = category;
+            FilterOperation = operation;
+            FilterValue = value;
+
+            await OnGetAsync();
+        }
+    }
+
+    public async Task ResetFilter()
+    {
+        FilterCategory = string.Empty;
+        FilterValue = string.Empty;
+
+        await OnGetAsync();
     }
 }
